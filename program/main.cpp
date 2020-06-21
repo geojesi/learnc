2e2dc307537fcd4de0eb99b5d030392ff1865e8d
geojesi <cutedasun@gmail.com>
2020-06-22 00:00:05 +0900

    Update main.cpp

--- program/main.cpp
+++ program/main.cpp
@@ -1,5 +1,12 @@
 #include <cstdio>
 
+int f(int k)
+{
+    if (k == 0)
+        return 1;
+    return 2*f(k-1);
+
+}
 int main()
 {
     char a[105] = {}, b[105] = {};
@@ -7,5 +14,13 @@ int main()
     int len1 = 0, len2 = 0;
     for (int i = 0; a[i] != 0; i++)
         len1++;
-    for (int i = 0; b[i] != 0; )
+    for (int i = 0; b[i] != 0; i++)
+        len2++;
+    int cnt = 0;
+    for (int i = 0; i < len1; i++)
+    {
+        if (a[i] == 1)
+            cnt += f(len1 - i + 1);
+
+    }
 }
