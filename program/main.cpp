816f1ef8b78b4aef461277822755c9300f8362e6
geojesi <cutedasun@gmail.com>
2020-06-21 23:43:01 +0900

    Create main.cpp

---
+++ program/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main()
+{
+    char a[105] = {}, b[105] = {};
+    scanf("%s %s", a, b);
+    int len1 = 0, len2 = 0;
+    for (int i = 0; a[i] != 0; i++)
+        len1++;
+    for (int i = 0; b[i] != 0; )
+}
